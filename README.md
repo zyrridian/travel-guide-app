7a4737f8a32a38f311f4733505faf01ffcac8264
zyrridian <rezkyaf246@gmail.com>
2024-12-26 22:25:25 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# travel_guide_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/travel_guide_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.travel_guide_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
