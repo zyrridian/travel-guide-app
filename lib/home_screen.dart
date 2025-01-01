7abc604a8e60fd9b54cca331ff5d4f56c2edb225
zyrridian <rezkyaf246@gmail.com>
2025-01-01 21:08:07 +0700

    feat: add popular place and banner card

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,144 +10,325 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Column(
-            children: [
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.orangeAccent,
-                  ),
-                  SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Good Morning',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Nunito',
-                            fontSize: 14,
-                          ),
-                        ),
-                        Text(
-                          'Hello, John!',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Nunito',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.notifications_outlined),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                ),
-                child: Row(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 8),
+                Row(
                   children: [
-                    Icon(Icons.search_outlined),
-                    SizedBox(width: 8),
-                    Text(
-                      'Search here...',
-                      style: TextStyle(
-                        color: Colors.grey,
+                    CircleAvatar(
+                      backgroundColor: Colors.orangeAccent,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Good Morning',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Nunito',
+                              fontSize: 14,
+                            ),
+                          ),
+                          Text(
+                            'Hello, John!',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Nunito',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
                       ),
-                    )
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.notifications_outlined),
+                    ),
                   ],
                 ),
-              ),
-              SizedBox(height: 16),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+                SizedBox(height: 16),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.search_outlined),
+                      SizedBox(width: 8),
+                      Text(
+                        'Search here...',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Chip(
+                        label: Text('Mountain'),
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(width: 8),
+                      Chip(
+                        label: Text('Beaches'),
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(width: 8),
+                      Chip(
+                        label: Text('Historical Buildings'),
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(width: 8),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
                   children: [
-                    Chip(
-                      label: Text('Mountain'),
-                      backgroundColor: Colors.white,
+                    Expanded(
+                      child: Text(
+                        'Popular Places',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
-                    SizedBox(width: 8),
-                    Chip(
-                      label: Text('Beaches'),
-                      backgroundColor: Colors.white,
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'view all',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.w300,
+                          fontSize: 16,
+                        ),
+                      ),
                     ),
-                    SizedBox(width: 8),
-                    Chip(
-                      label: Text('Historical Buildings'),
-                      backgroundColor: Colors.white,
-                    ),
-                    SizedBox(width: 8),
                   ],
                 ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      'Popular Places',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Nunito',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                      ),
-                    ),
+                SizedBox(
+                  height: 269,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: PopularPlaceCard(),
+                      );
+                    },
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'view all',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Nunito',
-                        fontWeight: FontWeight.w300,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 4),
-              Card(
+                ),
+                SizedBox(height: 32),
+                BannerCard(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BannerCard extends StatelessWidget {
+  const BannerCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.orangeAccent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
                 color: Colors.white,
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'images/cherries.jpg',
-                      width: 200,
-                      height: 200,
-                      fit: BoxFit.cover,
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: AssetImage('images/journey.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Top 10 Mountains in 2025',
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Plan your summer getaway now and explore breathtaking destination!',
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                      fontSize: 14,
+                      color: Colors.white.withOpacity(0.9),
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: BorderSide(color: Colors.orangeAccent),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      'Learn More',
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        color: Colors.black,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// FFB74D
+class PopularPlaceCard extends StatelessWidget {
+  const PopularPlaceCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        width: 240,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                'images/cherries.jpg',
+                width: double.infinity,
+                height: 140,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Mount Fuji',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                Icon(
+                  Icons.location_on,
+                  size: 18,
+                  color: Colors.grey[600],
+                ),
+                SizedBox(width: 4),
+                Text(
+                  "Kyoto, Japan",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 18,
+                      color: Colors.amber,
+                    ),
+                    SizedBox(width: 4),
                     Text(
-                      'Mt. Everest',
+                      "4.8",
                       style: TextStyle(
                         color: Colors.black,
-                        fontSize: 16,
-                        fontFamily: 'Nunito',
+                        fontSize: 14,
                         fontWeight: FontWeight.w500,
                       ),
-                    )
+                    ),
                   ],
                 ),
-              )
-            ],
-          ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.reviews,
+                      size: 18,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      "200 reviews",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
